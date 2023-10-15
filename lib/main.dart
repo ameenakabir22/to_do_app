dd546c943957e789006316d3e2cd7475a9e5a4f0
Amina_Kabir <ameenakabir22@gmail.com>
2023-10-15 00:24:44 +0000

    i added the hive dependncies

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/todo_folder/home_page.dart';
 
 void main() {
+  //initialize hive
+  Hive.initFlutter();
   runApp(const MyApp());
 }
+
+mixin Hive {}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
